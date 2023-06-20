9fd3bf77b39db4500ce3ca62ce20c93a1830ab83
sofia gomez m <112109214+sofiagomezm@users.noreply.github.com>
2023-06-20 16:33:08 -0400

    Update JsonUrlReader2.java
    v2

--- JsonUrlReader2.java
+++ JsonUrlReader2.java
@@ -1,53 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.apache.beam.examples;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.beam.examples.Aire;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-/**
- *
- * @author sofi
- */
 public class JsonUrlReader2 {
 
-    public JsonUrlReader2()
-    {
-        
-    }
-      
-    public ArrayList<Farmacia> cargarURL() throws StreamReadException, DatabindException, MalformedURLException, IOException {
-        String url = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php";
-        
-        ArrayList<Farmacia> aFarmacia = new ArrayList();
+    public JsonUrlReader2() {
 
-        //JsonNode node = new JsonUrlReader().get(url);
-        //System.out.println(node.toPrettyString());
-        
-        
+    }
+
+    public ArrayList<Farmacia> cargarURL() throws IOException {
+        String url = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php";
+
+        ArrayList<Farmacia> aFarmacia = new ArrayList<>();
+
         ObjectMapper mapper = new ObjectMapper();
 
-	System.out.println("Inicando mapper en JsonUrlReader");
+        System.out.println("Iniciando mapper en JsonUrlReader");
         JsonNode node = mapper.readTree(new URL(url));
 
         System.out.println("Finalizando mapper en JsonUrlReader");
-        
-        
+
         Iterator<JsonNode> it = node.iterator();
-        
-        Farmacia a = null;
-        a = new Farmacia();
+
+        Farmacia a = new Farmacia();
         a.setId("ID");
         a.setFecha("FECHA");
         a.setLocal_id("LOCAL_ID");
@@ -64,13 +46,11 @@ public class JsonUrlReader2 {
         a.setLocal_lat("LOCAL_LAT");
         a.setLocal_lng("LOCAL_LNG");
         a.setFuncionamiento_dia("FUNCIONAMIENTO_DIA");
-	    
-	    aFarmacia.add(a);    
+
+        aFarmacia.add(a);
 
         int i = 1;
-           
 
-        }   
         return aFarmacia;
     }
-//JsonUrlReader
+}
